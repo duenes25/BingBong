ef7a75dcc8ab3a3b6320fe2743650be76e5d687b
duenes25 <duenes25@yahoo.com>
2022-06-06 20:22:03 -0700

    Added Name position and number

--- app/src/main/java/com/bigbrain/coders/bingbong/MainActivity.kt
+++ app/src/main/java/com/bigbrain/coders/bingbong/MainActivity.kt
@@ -3,13 +3,19 @@ package com.bigbrain.coders.bingbong
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bigbrain.coders.bingbong.ui.theme.BingBongTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +25,9 @@ class MainActivity : ComponentActivity() {
             BingBongTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    playerCard()
                 }
             }
         }
@@ -39,5 +44,24 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     BingBongTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun playerCard(){
+    Card(
+        modifier= Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+            .clickable {},
+        elevation = 10.dp
+
+
+    ){
+        Column() {
+            Text("Name")
+            Text("position")
+            Text("Number")
+        }
     }
 }
